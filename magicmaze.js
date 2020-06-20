a093f653004728e7bcc079f298bbf93937400670
patrick xia <patrick.xia@gmail.com>
2020-06-20 17:13:44 -0400

    Remove unneeded comments from js

--- magicmaze.js
+++ magicmaze.js
@@ -291,7 +291,6 @@ function placeCharacter (obj, info) {
   const key = getKey(x, y)
   const top = obj.tops.get(key)
   const left = obj.lefts.get(key)
-  const el = $(`mm_token${info.token_id}`)
   const adjust = (CELL_SIZE - MEEPLE_SIZE) / 2
   obj.slideToObjectPos(`mm_token${info.token_id}`,
     'mm_area_scrollable_oversurface',
@@ -333,19 +332,6 @@ function (dojo, declare) {
       this.displayedEscape = false
     },
 
-    /*
-            setup:
-
-            This method must set up the game user interface according to current game situation specified
-            in parameters.
-
-            The method is called each time the game interface is displayed to a player, ie:
-            _ when the game starts
-            _ when a player refreshes the game page (F5)
-
-            "gamedatas" argument contains all datas retrieved by your "getAllDatas" PHP method.
-            */
-
     setup: function (gamedatas) {
       const game = this
 
@@ -361,7 +347,6 @@ function (dojo, declare) {
 
       this.flips = gamedatas.flips
 
-      // TODO: Set up your game interface here, according to "gamedatas"
       if (gamedatas.time_left) {
         this.deadline = (Date.now() / 1000.0) + parseFloat(gamedatas.time_left)
       }
@@ -533,50 +518,11 @@ function (dojo, declare) {
     // onUpdateActionButtons: in this method you can manage "action buttons" that are displayed in the
     //                        action status bar (ie: the HTML links in the status bar).
     //
-    onUpdateActionButtons: function (stateName, args) {
-      console.log('onUpdateActionButtons: ' + stateName)
-
-      if (this.isCurrentPlayerActive()) {
-        switch (stateName) {
-          /*
-                 Example:
-
-                 case 'myGameState':
-
-              // Add 3 action buttons in the action status bar:
-
-                    this.addActionButton( 'button_1_id', _('Button 1 label'), 'onMyMethodToCall1' );
-                    this.addActionButton( 'button_2_id', _('Button 2 label'), 'onMyMethodToCall2' );
-                    this.addActionButton( 'button_3_id', _('Button 3 label'), 'onMyMethodToCall3' );
-                    break;
-                    */
-        }
-      }
-    },
-
-    /// ////////////////////////////////////////////////
-    /// / Utility methods
-
-    /*
-
-            Here, you can defines some utility methods that you can use everywhere in your javascript
-            script.
-
-*/
+    onUpdateActionButtons: function (stateName, args) {},
 
     /// ////////////////////////////////////////////////
     /// / Player's action
 
-    /*
-
-            Here, you are defining methods to handle player's action (ex: results of mouse click on
-            game objects).
-
-            Most of the time, these methods:
-            _ check the action is possible at this game state.
-            _ make a call to the game server
-
-*/
     onCreateTile: function (tileID, x, y) {
       // TODO checkAction
       this.ajaxcall('/magicmaze/magicmaze/placeTile.html',
@@ -589,52 +535,10 @@ function (dojo, declare) {
           console.log(result)
         }, function (error) { console.log(error) })
     },
-    /* Example:
-
-        onMyMethodToCall1: function( evt )
-        {
-            console.log( 'onMyMethodToCall1' );
-
-      // Preventing default browser reaction
-            dojo.stopEvent( evt );
-
-      // Check that this action is possible (see "possibleactions" in states.inc.php)
-            if( ! this.checkAction( 'myAction' ) )
-            {   return; }
-
-            this.ajaxcall( "/magicmaze/magicmaze/myAction.html", {
-                                                                    lock: true,
-                                                                    myArgument1: arg1,
-                                                                    myArgument2: arg2,
-                                                                    ...
-                                                                 },
-                         this, function( result ) {
-
-      // What to do after the server call if it succeeded
-      // (most of the time: nothing)
-
-                         }, function( is_error) {
-
-      // What to do after the server call in anyway (success or failure)
-      // (most of the time: nothing)
-
-                         } );
-        },
-
-*/
 
     /// ////////////////////////////////////////////////
     /// / Reaction to cometD notifications
 
-    /*
-            setupNotifications:
-
-            In this method, you associate each of your game notifications with your local method to handle it.
-
-            Note: game notification names correspond to "notifyAllPlayers" and "notifyPlayer" calls in
-                  your magicmaze.game.php file.
-
-*/
     notif_tileAdded: function (notif) {
       placeTile(this, notif.args)
       drawProperties(this, notif.args.clickables)
@@ -684,35 +588,6 @@ function (dojo, declare) {
       dojo.subscribe('newZombie', this, 'notif_newZombie')
       dojo.subscribe('newUsed', this, 'notif_newUsed')
       dojo.subscribe('attention', this, 'notif_attention')
-
-      // TODO: here, associate your game notifications with local methods
-
-      // Example 1: standard notification handling
-      // dojo.subscribe( 'cardPlayed', this, "notif_cardPlayed" );
-
-      // Example 2: standard notification handling + tell the user interface to wait
-      //            during 3 seconds after calling the method in order to let the players
-      //            see what is happening in the game.
-      // dojo.subscribe( 'cardPlayed', this, "notif_cardPlayed" );
-      // this.notifqueue.setSynchronous( 'cardPlayed', 3000 );
-      //
     }
-
-    // TODO: from this point and below, you can write your game notifications handling methods
-
-    /*
-        Example:
-
-        notif_cardPlayed: function( notif )
-        {
-            console.log( 'notif_cardPlayed' );
-            console.log( notif );
-
-      // Note: notif.args contains the arguments specified during you "notifyAllPlayers" / "notifyPlayer" PHP call
-
-      // TODO: play the card in the user interface.
-        },
-
-*/
   })
 })
